2f3e6cd9436d87ed9d3815cca7eb36e03b0ad414
Miklos Gabor <gmiki91@gmail.com>
2026-02-23 12:25:46 +0100

    add castling and turn order to exercise

--- src/app/exercises/exercise-creator/exercise-creator.ts
+++ src/app/exercises/exercise-creator/exercise-creator.ts
@@ -1,31 +1,62 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild, signal, model } from '@angular/core';
 import { ChessBoard } from '../../chess-board/chess-board';
 import { Color, Key, Role } from '@lichess-org/chessground/types';
 import { Config } from '@lichess-org/chessground/config';
 import { Chess } from 'chess.js';
-
+import { ExerciseService } from '../exercise.service';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-exercise-creator',
-  imports: [ChessBoard],
+  imports: [ChessBoard, MatRadioModule, MatCheckboxModule, FormsModule],
   templateUrl: './exercise-creator.html',
   styleUrl: './exercise-creator.scss',
 })
 export class ExerciseCreator {
   @ViewChild('chessBoard') chessBoard!: ChessBoard;
+  exerciseService = inject(ExerciseService);
+  whiteCastlingKingSide = model(true);
+  whiteCastlingQueenSide = model(true);
+  blackCastlingKingSide = model(true);
+  blackCastlingQueenSide = model(true);
+  turnOrder = model<'w' | 'b'>('w');
   private chess = new Chess();
 
+  boardConfig = signal<Config>({
+    fen: this.chess.fen(),
+    orientation: 'white',
+    coordinates: false,
+    movable: {
+      free: true,
+      events: {
+        after: (orig, dest) => this.handleMove(orig, dest),
+      },
+    },
+    draggable: {
+      enabled: true,
+      deleteOnDropOff: true,
+    },
+    highlight: {
+      lastMove: false,
+    },
+  });
+
   ngAfterViewInit(): void {
-    this.configureBoard();
     const board = this.chessBoard.boardElement.nativeElement;
     this.addDragListener(board);
     this.addDropListener(board);
   }
 
   handleMove(orig: Key, dest: Key) {}
-  
+
   onDragStart(event: DragEvent, role: Role, color: Color) {
     event.dataTransfer?.setData('role', role);
     event.dataTransfer?.setData('color', color);
+  }
+
+  save() {
+    this.exerciseService.addExercise(this.buildFen());
   }
 
   private onDrop(event: DragEvent) {
@@ -39,28 +70,6 @@ export class ExerciseCreator {
     }
   }
 
-  private configureBoard() {
-    const boardConfig: Config = {
-      fen: this.chess.fen(),
-      orientation: 'white',
-      coordinates: true,
-      movable: {
-        free: true,
-        events: {
-          after: (orig, dest) => this.handleMove(orig, dest),
-        },
-      },
-      draggable: {
-        enabled: true,
-        deleteOnDropOff: true,
-      },
-      highlight: {
-        lastMove: false,
-      },
-    };
-    this.chessBoard.api?.set(boardConfig);
-  }
-
   private addDragListener(el: HTMLElement) {
     el.addEventListener('dragover', (e: DragEvent) => {
       e.preventDefault();
@@ -71,5 +80,14 @@ export class ExerciseCreator {
     el.addEventListener('drop', (e: DragEvent) => {
       this.onDrop(e);
     });
+  }
+
+  private buildFen(): string {
+    const castling =
+      (this.whiteCastlingKingSide() ? 'K' : '') +
+        (this.whiteCastlingQueenSide() ? 'Q' : '') +
+        (this.blackCastlingKingSide() ? 'k' : '') +
+        (this.blackCastlingQueenSide() ? 'q' : '') || '-';
+    return this.chessBoard.api.getFen() + ` ${this.turnOrder()} ${castling} - 0 1`;
   }
 }
