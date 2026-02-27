fc29794a84cf6cf54a3c391be7bfbbfdbe3db38f
Miklos Gabor <gmiki91@gmail.com>
2026-02-27 23:13:04 +0100

    update classroom, lobby

--- src/app/features/classroom/classroom/classroom.ts
+++ src/app/features/classroom/classroom/classroom.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StudentTable } from '../student-table/student-table';
 import { TeacherTable } from '../teacher-table/teacher-table';
+import { Exercise } from '../../../shared/models/exercise.model';
+import { ExerciseList as List } from '../../../shared/models/exercise-list.model';
+import { ExerciseList } from '../../../shared/components/exercise-list/exercise-list';
+import { ExerciseService } from '../../../core/services/exercise.service';
+import { ClassroomService } from '../classroom.service';
 
 @Component({
   selector: 'app-classroom',
-  imports: [StudentTable,TeacherTable],
+  imports: [StudentTable, TeacherTable, ExerciseList],
   templateUrl: './classroom.html',
   styleUrl: './classroom.scss',
 })
 export class Classroom {
-
+  exerciseService = inject(ExerciseService);
+  classroomService = inject(ClassroomService);
+  loadExerciseToDemo(ex: Exercise) {this.classroomService.loadedExercise.set(ex)}
+  loadListToAll(list: List) {this.classroomService.loadedList.set(list.exercises)}
 }
--- src/app/features/lobby/lobby.ts
+++ src/app/features/lobby/lobby.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { ExerciseService } from '../../core/services/exercise.service';
 
 @Component({
   selector: 'app-lobby',
-  imports: [MatCardModule,MatIcon,RouterLink],
+  imports: [MatCardModule, MatIcon, RouterLink],
   templateUrl: './lobby.html',
   styleUrl: './lobby.scss',
 })
-export class Lobby {
+export class Lobby implements OnInit {
+  exerciseService = inject(ExerciseService);
+
+  ngOnInit(): void {
+    this.exerciseService.loadExerciseLists();
+  }
 
 }
