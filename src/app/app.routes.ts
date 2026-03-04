21dd10ec388db81ad0120dc8d16dadc4daba1f57
Miklos Gabor <gmiki91@gmail.com>
2026-03-04 19:57:13 +0100

    add lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,48 @@
 import { Routes } from '@angular/router';
-
-import { ExercisesLayout } from './features/exercises/exercises-layout/exercises-layout';
-import { BoardCreator } from './features/exercises/board-creator/board-creator';
-import { ExerciseCreator } from './features/exercises/exercise-creator/exercise-creator';
-import { Lobby } from './features/lobby/lobby';
 import { validListGuard } from './core/guards/valid-list.guard';
 import { validExerciseGuard } from './core/guards/valid-exercise.guard';
-import { Classroom } from './features/classroom/classroom/classroom';
-import { JoinScreen } from './features/classroom/join-screen/join-screen';
-import { StudentView } from './features/classroom/student-view/student-view';
 import { studentGuard } from './core/guards/student.guard';
 
 export const routes: Routes = [
-
-  { path: '', component: Lobby },
-  { path: 'join', component: JoinScreen },
-  { path: 'classroom', component: Classroom },
-   { path: 'student', component: StudentView, canActivate: [studentGuard] },
+  { path: '', loadComponent: () => import('./features/lobby/lobby').then((m) => m.Lobby) },
+  {
+    path: 'join',
+    loadComponent: () =>
+      import('./features/classroom/join-screen/join-screen').then((m) => m.JoinScreen),
+  },
+  {
+    path: 'classroom',
+    loadComponent: () =>
+      import('./features/classroom/classroom/classroom').then((m) => m.Classroom),
+  },
+  {
+    path: 'student',
+    loadComponent: () =>
+      import('./features/classroom/student-view/student-view').then((m) => m.StudentView),
+    canActivate: [studentGuard],
+  },
   {
     path: 'exercises',
-    component: ExercisesLayout,
+    loadComponent: () =>
+      import('./features/exercises/exercises-layout/exercises-layout').then(
+        (m) => m.ExercisesLayout,
+      ),
+
     children: [
-      { path: 'create/:listId', component: BoardCreator, canActivate:[validListGuard] },
-      { path: 'edit/:exerciseId', component: ExerciseCreator, canActivate:[validExerciseGuard] },
+      {
+        path: 'create/:listId',
+        loadComponent: () =>
+          import('./features/exercises/board-creator/board-creator').then((m) => m.BoardCreator),
+        canActivate: [validListGuard],
+      },
+      {
+        path: 'edit/:exerciseId',
+        loadComponent: () =>
+          import('./features/exercises/exercise-creator/exercise-creator').then(
+            (m) => m.ExerciseCreator,
+          ),
+        canActivate: [validExerciseGuard],
+      },
     ],
   },
 ];
