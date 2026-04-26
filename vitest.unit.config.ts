8ec5a2ef8915e218101ac048a40fe26fba8f4331
Miklos Gabor <gmiki91@gmail.com>
2026-04-26 12:29:47 +0200

    fix vite

--- vitest.unit.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    globals: true,
-    include: ['src/**/*.spec.ts'],
-    exclude: ['src/app/tests/integration/*.spec.ts'],
-  },
-});
