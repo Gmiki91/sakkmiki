316d732738ea7543b6eb2604d94703d5f74ca78b
Miklos Gabor <gmiki91@gmail.com>
2026-06-01 20:31:49 +0200

    fix .env, ci, angular.json file

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://ljusxjbumgmwjfphsiey.supabase.co',
   supabaseKey: 'sb_publishable_Yg7Y5J6xuCj3cIOgRrI8Xw_fVE9Rzx8',
 };
